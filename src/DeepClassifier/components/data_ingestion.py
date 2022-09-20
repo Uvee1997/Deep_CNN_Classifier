2235d9d0e56c7dd612aad90c18a5df55715fccc0
Uvee1997 <yuvraj_patil9721@yahoo.com>
2022-09-20 20:39:44 +0530

    lint update

--- src/DeepClassifier/components/data_ingestion.py
+++ src/DeepClassifier/components/data_ingestion.py
@@ -7,6 +7,7 @@ from DeepClassifier.utils import get_size
 from tqdm import tqdm
 from pathlib import Path
 
+
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
         self.config = config
@@ -16,12 +17,12 @@ class DataIngestion:
         if not os.path.exists(self.config.local_data_file):
             logger.info("Download started...")
             filename, headers = request.urlretrieve(
-                url = self.config.source_URL,
-                filename = self.config.local_data_file
+                url=self.config.source_URL,
+                filename=self.config.local_data_file
             )
             logger.info(f"{filename} download! with following info: \n{headers}")
         else:
-            logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")    
+            logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")
 
     def _get_updated_list_of_files(self, list_of_files):
         return [f for f in list_of_files if f.endswith(".jpg") and ("Cat" in f or "Dog" in f)]
@@ -30,7 +31,7 @@ class DataIngestion:
         target_filepath = os.path.join(working_dir, f)
         if not os.path.exists(target_filepath):
             zf.extract(f, working_dir)
-        
+
         if os.path.getsize(target_filepath) == 0:
             os.remove(target_filepath)
             logger.info(f"removing file:{target_filepath} of size: {get_size(Path(target_filepath))}")
