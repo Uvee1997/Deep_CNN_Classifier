00fdf24a84736f6198cf3b3938e6f6cdff5d25bc
Uvee1997 <yuvraj_patil9721@yahoo.com>
2022-09-20 21:26:54 +0530

    Setup Completed

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "FSDS_NOV_deepCNNClassifier"
-AUTHOR_USER_NAME = "c17hawke"
-SRC_REPO = "deepClassifier"
-AUTHOR_EMAIL = "sunny.c17hawke@gmail.com"
+REPO_NAME = "Deep_CNN_Classifier"
+AUTHOR_USER_NAME = "Uvee1997"
+SRC_REPO = "DeepClassifier"
+AUTHOR_EMAIL = "uv.patil2010@gmail.com"
 
 setuptools.setup(
     name=SRC_REPO,
